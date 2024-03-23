e0555d698001e32019281e935b2190d94d794948
pablokhmel <pablokhmelevskiy@gmail.com>
2024-03-23 12:40:22 +0100

    Initial Commit

---
+++ test-task/test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_taskApp.swift
+//  test-task
+//
+//  Created by MacBook on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
